0945795fdaad3456b66a5d9a8d44caeb28d7cb7a
wbz002 <1041128051@qq.com>
2018-10-10 22:22:31 -0400

    解决重复重启osd的问题

--- restart_alldown_osd.sh
+++ restart_alldown_osd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for down_osd in $(ceph osd tree | awk '/down/ {print $1}')
+for down_osd in $(ceph osd tree | awk '/down/ {print $1}' | sort | uniq)
 do
   host=$(ceph osd find $down_osd | awk -F\" '$2 ~ /host/ {print $4}')
   echo ssh $host "systemctl restart ceph-osd@${down_osd}.service"
